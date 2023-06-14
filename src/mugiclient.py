a6bf07838c26f053b31bac72e56f7b450207f31a
Manitary <pak.ska@gmail.com>
2023-06-14 02:07:58 +0100

    Add file upload option
    By default, the bot will send local assets
    instead of sending messages containing asset URLs
    
    Add mutually exclusive optional arguments to use Imgur/Github assets
    
    General refactor and slightly more docstrings

--- src/mugiclient.py
+++ src/mugiclient.py
@@ -35,44 +35,131 @@ class MugiError(Exception):
 
 
 class AssetType(Enum):
+    """Which assets mugi is going to use."""
+
     GITHUB = auto()
     IMGUR = auto()
+    FILE = auto()
+
+
+@dataclass
+class MugiMessage:
+    """Contents of the message to be sent.
+
+    content: text of the message
+    file: file path (Path or str) of the file to send"""
+
+    content: str = ""
+    file: discord.File | None = None
+
+    @property
+    def contents(self) -> dict[str, str | discord.File | None]:
+        """Return the contents as dictionary."""
+        # dict keys must match ``discord.Webhook.send`` arguments
+        data = {
+            "content": self.content,
+            "file": self.file,
+        }
+        return {k: v for k, v in data.items() if v}
+
+
+def build_imgur_message(commentface: str) -> MugiMessage:
+    """Return the contents of the message based on Imgur assets."""
+    return MugiMessage(content=COMMENTFACES_URL.get(commentface, ""))
+
+
+def build_github_message(commentface: str) -> MugiMessage:
+    """Return the contents of the message based on Github assets."""
+    commentface_path = get_commentface_file_path(commentface)
+    if not commentface_path:
+        return MugiMessage()
+    relative_path = "/".join(commentface_path.parts[2:])  # remove src/assets
+    url = GITHUB_PREVIEW_URL.format(relative_path=relative_path).replace(" ", "%20")
+    return MugiMessage(content=url)
+
+
+def build_file_message(commentface: str) -> MugiMessage | None:
+    """Return the contents of the message based on local assets."""
+    commentface_path = get_commentface_file_path(commentface)
+    if not commentface_path:
+        return None
+    return MugiMessage(file=discord.File(commentface_path))
+
+
+BUILD_MESSAGE = {
+    AssetType.GITHUB: build_github_message,
+    AssetType.FILE: build_file_message,
+    AssetType.IMGUR: build_imgur_message,
+}
 
 
 class Mugiwait(discord.Client):
-    _asset_type: AssetType = AssetType.GITHUB
+    """Mugified discord client.
+
+    Attributes:
+    * ``_asset_type``
+
+        The type of asset to use during the session.
+
+    Defaults to FILE, can be changed to IMGUR or GITHUB with optional arguments
+    when launching mugi (-i and -g, respectively).
+
+    Accessed via the ``asset_type`` property.
+
+    Methods:
+    * ``build_messages_from_message``
+
+        Process the contents of a valid message to extract commentface code and additional text.
+
+        Return a list of messages to send based on parsed contents and asset type of choice.
+    """
+
+    _asset_type: AssetType = AssetType.FILE
 
     @property
     def asset_type(self) -> AssetType:
+        """The type of asset to use for the session."""
         return self._asset_type
 
     @asset_type.setter
     def asset_type(self, value: AssetType) -> None:
         self._asset_type = value
 
+    def build_messages_from_message(self, text: str) -> list[MugiMessage]:
+        """Return a list of message contents to send and replace the processed input text."""
+        messages: list[MugiMessage] = []
+        prefix = text[0]
+        match = RE_COMMENTFACE[prefix].match(text)
+        if not match:
+            logger.debug("No match found")
+            return messages
+        match_dict = match.groupdict()
+        commentface = match_dict.get("commentface", "")
+        if not commentface:
+            logger.debug("No commentface found")
+            return messages
 
-# Commentface retrieval
+        commentface_message = BUILD_MESSAGE[self.asset_type](commentface)
+        if not commentface_message:
+            logger.debug("No commentface found")
+            return messages
 
+        overlay = match_dict.get("overlay", "")
+        commentface_message.content = overlay
+        messages.append(commentface_message)
 
-def get_url_imgur(commentface: str) -> str:
-    """Return the Imgur URL matching the commentface code."""
-    return COMMENTFACES_URL.get(commentface, "")
+        if hovertext := match_dict.get("hovertext", ""):
+            messages.append(MugiMessage(content=hovertext))
 
-
-IMAGES_PATHS = itertools.chain(
-    (
-        "src/assets/preview/*/{commentface}.*",
-        "src/assets/source_seasonal_faces/hallOfFame/{commentface}.*",
-        tuple(itertools.chain("src/assets/source_seasonal_faces/{season}/")),
-    )
-)
+        return messages
 
 
 def get_seasonal_path(commentface: str) -> Path:
     """Return the path of a seasonal commentface.
 
     If not available, raise an exception.
-    Only return the latest season."""
+
+    Only return the latest possible season."""
     for season in SEASONS_LIST:
         if path := list(
             Path().glob(
@@ -83,45 +170,34 @@ def get_seasonal_path(commentface: str) -> Path:
     raise MugiError()
 
 
-def get_url_github(commentface: str) -> str:
-    """Return the Github URL matching the commentface code."""
+def get_commentface_file_path(commentface: str) -> Path | None:
+    """Return the asset file path for the given commentface code, if it exists."""
     if commentface.startswith("seasonal"):
         try:
-            commentface_paths = [get_seasonal_path(commentface)]
+            return get_seasonal_path(commentface)
         except MugiError:
             logger.warning("Seasonal commentface %s not found", commentface)
-            return ""
-    else:
-        commentface_paths = list(
-            itertools.chain(
-                Path().glob(
-                    f"src/assets/preview/*/{commentface}.*",
-                ),
-                Path().glob(
-                    f"src/assets/source_seasonal_faces/hallOfFame/*/{commentface}.*",
-                ),
-            )
+            return None
+    commentface_paths = list(
+        itertools.chain(
+            Path().glob(
+                f"src/assets/preview/*/{commentface}.*",
+            ),
+            Path().glob(
+                f"src/assets/source_seasonal_faces/hallOfFame/*/{commentface}.*",
+            ),
         )
+    )
     if not commentface_paths:
-        logger.debug("Commentface %s not found", commentface)
-        return ""
+        logger.info("Commentface %s not found", commentface)
+        return None
     if len(commentface_paths) > 1:
         logger.warning("%d valid paths found", len(commentface_paths))
-    relative_path = "/".join(commentface_paths[0].parts[2:])  # remove src/assets
-    url = GITHUB_PREVIEW_URL.format(relative_path=relative_path).replace(" ", "%20")
-    return url
-
-
-MESSAGE_FUNCTION = {
-    AssetType.GITHUB: get_url_github,
-    AssetType.IMGUR: get_url_imgur,
-}
-
-# Other
+    return commentface_paths[0]
 
 
 def is_valid_message(message: discord.Message, client: discord.Client) -> bool:
-    """Return whether the bot will further analyse the message contents."""
+    """Return whether mugi will further analyse the message contents."""
     if message.webhook_id:
         logger.debug("Ignoring webhook message")
         return False
@@ -150,43 +226,3 @@ async def get_webhook(channel: discord.TextChannel, hook_name: str) -> discord.W
             )
 
     return hook
-
-
-@dataclass
-class ParsedMessage:
-    commentface: str = ""
-    overlay: str = ""
-    hovertext: str = ""
-
-
-def parse_code(assets: AssetType, text: str) -> ParsedMessage:
-    """Return the commentface and other text matching the input."""
-    prefix = text[0]
-    match = RE_COMMENTFACE[prefix].match(text)
-    if not match:
-        logger.debug("No match found")
-        return ParsedMessage()
-    match_dict = match.groupdict()
-    commentface = match_dict.get("commentface", "")
-    if commentface:
-        commentface = MESSAGE_FUNCTION[assets](commentface)
-        logger.info("URL found: %s", commentface)
-    overlay = match_dict.get("overlay", "")
-    hovertext = match_dict.get("hovertext", "")
-    message = ParsedMessage(
-        commentface=commentface, overlay=overlay, hovertext=hovertext
-    )
-    logger.info("Message contents: %s", message)
-    return message
-
-
-def compose_messages(assets: AssetType, text: str) -> list[str]:
-    """Return a list of message contents to send in the channel."""
-    contents: list[str] = []
-    parsed_text = parse_code(assets=assets, text=text)
-    if not parsed_text.commentface:
-        return contents
-    contents.append(parsed_text.overlay)
-    contents.append(parsed_text.commentface)
-    contents.append(parsed_text.hovertext)
-    return contents
